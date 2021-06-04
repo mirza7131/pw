b0f0299ad0bd63efa08596227b6f22d4200a98db
mirzaaahsan <Owner@DESKTOP-8643VQ8>
2021-06-05 01:00:57 +0500

    fix

--- push.js
+++ push.js
@@ -10,11 +10,17 @@ let vapidkey = {
 
   let sub =
   /// ahsan 
- {
-    endpoint:"https://fcm.googleapis.com/fcm/send/e7Ed1d0yb7s:APA91bHpaSDxLV7W4U6l79IQLm8v6eynLSuc-ahMxBKt1_ChOFmm02EmS_4SQDS_R8BqDIF_RN2lMOFgaGWLNTE8q9toNf2BEyZiATbNvEYMxqJAtJiA3IaD82r8Ri8tHj9O9SBwgZ6k",
-  expirationTime:null,
-  keys:{p256dh:"BPVGrmIdkoMugyIy7edDua7u4XsJbtfI05ZlK5A9hUc2R9gwNFW5IzhCoCGOi8W1hvEixmDxr6boEmgMqnkrKPo",
-  auth:"jEit-PDOv7lY4WJurrdxJg"}}
+//  {
+//     endpoint:"https://fcm.googleapis.com/fcm/send/e7Ed1d0yb7s:APA91bHpaSDxLV7W4U6l79IQLm8v6eynLSuc-ahMxBKt1_ChOFmm02EmS_4SQDS_R8BqDIF_RN2lMOFgaGWLNTE8q9toNf2BEyZiATbNvEYMxqJAtJiA3IaD82r8Ri8tHj9O9SBwgZ6k",
+//   expirationTime:null,
+//   keys:{p256dh:"BPVGrmIdkoMugyIy7edDua7u4XsJbtfI05ZlK5A9hUc2R9gwNFW5IzhCoCGOi8W1hvEixmDxr6boEmgMqnkrKPo",
+//   auth:"jEit-PDOv7lY4WJurrdxJg"}}
+
+//usman
+{endpoint:"https://fcm.googleapis.com/fcm/send/dOvxbXf6ues:APA91bHavGY4pdQvOg5MhkXiSkyktXSKbcAP1Rt9VPPb64oE9kFEz0WgTobKn7Duo5m3XHDSo8W2n2MNZ1Rxysj_2PCYaGcGZVzwpmXh1mC2f-hC5gPGQlVWUtYkpaXgDU6bduUyndq6",
+expirationTime:null,keys:{p256dh:"BMDTwlkC1CKbHn7_jfWB90qGsmnFQJTUVkwQryKFeXFkoKBva6YkF2UD2bFzCn_qzAFBI8b9dqWnohfUsr_JmN8",
+auth:"EVO-teyxna--NVyF8biaqA"}}
+
   ///// mehran
   // {endpoint:"https://fcm.googleapis.com/fcm/send/eId2AYwiu1A:APA91bHrceYFGJQjxVC87LHotgv85ynCtNU4ZKiJo1CMUZm4_RyTQxsvrWl72gQdKvGq21Xhno_aSuwZosLeRKW4HHdABGx6a5_X6AfQdEgK-sYiigTryptWY6gEtiA6quPMc0iqKbE1",
   // expirationTime:null,
