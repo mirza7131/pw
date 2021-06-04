2edbd8f12ecc5addbb9686c4291ecc21184b8070
mirzaaahsan <Owner@DESKTOP-8643VQ8>
2021-06-04 14:48:51 +0500

    fix

--- push.js
+++ push.js
@@ -11,12 +11,9 @@ let vapidkey = {
   let sub = 
   {
     endpoint:"https://fcm.googleapis.com/fcm/send/e7Ed1d0yb7s:APA91bHpaSDxLV7W4U6l79IQLm8v6eynLSuc-ahMxBKt1_ChOFmm02EmS_4SQDS_R8BqDIF_RN2lMOFgaGWLNTE8q9toNf2BEyZiATbNvEYMxqJAtJiA3IaD82r8Ri8tHj9O9SBwgZ6k",
-    expirationTime:null,
-    keys:
-    {
-    p256dh:"BPVGrmIdkoMugyIy7edDua7u4XsJbtfI05ZlK5A9hUc2R9gwNFW5IzhCoCGOi8W1hvEixmDxr6boEmgMqnkrKPo",
-    auth:"jEit-PDOv7lY4WJurrdxJg"}
-    }
+  expirationTime:null,
+  keys:{p256dh:"BPVGrmIdkoMugyIy7edDua7u4XsJbtfI05ZlK5A9hUc2R9gwNFW5IzhCoCGOi8W1hvEixmDxr6boEmgMqnkrKPo",
+  auth:"jEit-PDOv7lY4WJurrdxJg"}}
 
   push.sendNotification(sub,'test message');
   console.log(  push.sendNotification(sub,'test message'),'okk')
