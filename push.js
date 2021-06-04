8338c897d981458bb5fa1b9737968b7b9781c265
mirzaaahsan <Owner@DESKTOP-8643VQ8>
2021-06-04 14:41:59 +0500

    fix

--- push.js
+++ push.js
@@ -9,11 +9,14 @@ let vapidkey = {
   push.setVapidDetails('mailto:mirzaaahsan@gmail.com', vapidkey.publicKey,vapidkey.privateKey)
 
   let sub = 
-  {endpoint:`https://fcm.googleapis.com/fcm/send/evZihAkrV0E:APA91bGBmvvPNOz08LfBA0Fj386GSZHKzvtkl6kCvInTy30zUSpTyZrQ9jG28LDfjW2heeA9ORK9JLNLYoFAddXfzW3LcuCYnfmvCZxFqazaieAnmMnx_sgSjqyHs3iOOkUtUIP5Dsf9`,
-  expirationTime:null,
-  keys:{
-    p256dh:'BLwr-1UVnYopDJIsO2V1VRNcvBtSAvWlLdSP96PdchcSN7kbLOqc35bL_5rS681dBCKn9Z2YXZYidT2pN41qAvM',
-  auth:'hbIQiyhVRb4aZlZKl2CUpQ'}}
+  {
+    endpoint:"https://fcm.googleapis.com/fcm/send/e7Ed1d0yb7s:APA91bHpaSDxLV7W4U6l79IQLm8v6eynLSuc-ahMxBKt1_ChOFmm02EmS_4SQDS_R8BqDIF_RN2lMOFgaGWLNTE8q9toNf2BEyZiATbNvEYMxqJAtJiA3IaD82r8Ri8tHj9O9SBwgZ6k",
+    expirationTime:null,
+    keys:
+    {
+    p256dh:"BPVGrmIdkoMugyIy7edDua7u4XsJbtfI05ZlK5A9hUc2R9gwNFW5IzhCoCGOi8W1hvEixmDxr6boEmgMqnkrKPo",
+    auth:"jEit-PDOv7lY4WJurrdxJg"}
+    }
 
   push.sendNotification(sub,'test message');
   console.log(  push.sendNotification(sub,'test message'),'okk')
