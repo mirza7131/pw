29744ce0c2c3ff9580d6003c0c6f7dd3cf635981
mirzaaahsan <Owner@DESKTOP-8643VQ8>
2021-06-04 14:47:05 +0500

    fix

--- sw.js
+++ sw.js
@@ -1,14 +1,14 @@
 self.addEventListener('push', function(e) {
     var options = {
-      body: 'This notification was generated from a push!',
+      body: 'This notification was generated from a push notification!',
       icon: 'images/example.png',
       vibrate: [100, 50, 100],
       data: {
         dateOfArrival: Date.now(),
-        primaryKey: '2'
+        primaryKey: '10'
       },
       actions: [
-        {action: 'explore', title: 'Explore this new world',
+        {action: 'https://www.google.com/', title: 'Explore this new world',
           icon: 'images/checkmark.png'},
         {action: 'close', title: 'Close',
           icon: 'images/xmark.png'},
